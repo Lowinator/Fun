bce71c50443076d7133912a60aecfd6078dcf163
David <divadglavas@gmail.com>
2017-10-10 22:47:28 +0200

    Completed findMin

--- algs/src/findMin.java
+++ algs/src/findMin.java
@@ -2,12 +2,8 @@
  * A cache aware algorithm which, given two arrays X, Y and a 2 dim function f, computes the smallest
  * possible value f(X[i], Y[j]) by trying out all (i, j) pairs in an IO efficient manner.
  * Number of IOs is O(n^2/M*B) where M is the memory/cache size and B the block size.
- *
- * TODO A cache aware algorithm which, given two arrays X, Y and a 2 dim function, computes a pair (i, j)
- * such that f(X[i], Y[i]) is the smallest value f returns for all possible (i, j) pairs.
- *
- *
  */
+
 public class findMin {
 
     public static void main(String[] args) {
@@ -16,23 +12,28 @@ public class findMin {
         int[] Y = {6, 7, 8, 9, 10};
 
         // define f
+        BinaryIntegerOperation f = (a, b) -> a + b;
 
-//        int minValue = findMin(X, Y, f);
+        int minValue = findMin(X, Y, f);
 
-//        System.out.println("Minimum achievable value of f for all pairs is " + minValue);
+        System.out.println("Minimum achievable value of f for all pairs is " + minValue);
     }
 
-    // TODO get familiar with lambda expressions lol
-/*    static int findMin(int[] X, int[] Y, f) {
+    static int findMin(int[] X, int[] Y, BinaryIntegerOperation f) {
 
         int currentMin = Integer.MAX_VALUE;
         for (int i = 0; i < X.length; i++) {
             for (int j = 0; j < Y.length; j++) {
-                currentMin = Integer.min(currentMin, f(X[i], Y[j]))
+                currentMin = Integer.min(currentMin, f.operation(X[i], Y[j]));
             }
         }
 
         return currentMin;
     }
-*/
+
+}
+
+@FunctionalInterface
+interface BinaryIntegerOperation {
+    int operation(int a, int b);
 }
