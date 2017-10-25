6e98fc1546a66e78e4fa0da7a6ea7b15d8f8932c
David <divadglavas@gmail.com>
2017-10-25 22:08:02 +0200

    Initial complete (untested) version of the count min sketch.

--- fun/src/streamingAlgorithms/CountMinSketch.java
+++ fun/src/streamingAlgorithms/CountMinSketch.java
@@ -14,25 +14,10 @@ public class CountMinSketch {
 
     public static void main(String[] args) {
 
-    }
-
-
-    /**
-     * Queries the sketch.
-     *
-     * The results of all parallel executions (rows in sketch) are being compared, the minimal entry in column j
-     * will be returned as all of the approximations must be overestimates.
-     * @param sketch Target sketch.
-     * @param j Item whose frequency will be approximated.
-     * @return Approximation of the frequency for the given item.
-     */
-    int querrySketch(int[][] sketch, int j) {
-
-        int min = Integer.MAX_VALUE;
-
-        // TODO hf accessing the hash functions... genius.
+        // TODO tests!
 
     }
+
     /**
      * Randomized algorithm which returns a sketch which can be queried for an approximation of
      * the frequency of each item in the given stream.
@@ -41,7 +26,7 @@ public class CountMinSketch {
      * The algorithm starts by randomly distributing (pairwise independent hash family) items of the given stream among k groups.
      * The frequency of an item j is approximated to be the sum of all frequencies within the group that j is
      * located in.
-     * The described routine above is run t times in parallel, each row of the returned 2 dim array
+     * The described routine above is run t times in parallel, each row of the 2 dim array
      * represents one run, we obtain the best solution for a given item j by querying a column of the array
      * and returning the minimal entry of that column (since all approximations are always overestimates).
      *
@@ -57,7 +42,7 @@ public class CountMinSketch {
      * @param delt Determines the probability with which the returned solution has the accuracy specified by eps.
      * @return A sketch which can be queried for frequency approximations.
      */
-    int[][] computeSketch(Item[] stream, double eps, double delt) {
+    FrequencySketch computeSketch(Item[] stream, double eps, double delt) {
 
         int k = (int) Math.ceil(2/eps);
 
@@ -81,7 +66,50 @@ public class CountMinSketch {
                 sketch[i][h[i].hash(j.ID)] += j.weight;
             }
 
-        return sketch;
+        // returns the required values and the hash functions necessary to query for them properly
+        return new FrequencySketch(sketch, h);
+    }
+
+    // encapsulates the result of the algorithm, it can be queried for approximations
+    private static class FrequencySketch {
+
+        // hash functions are necessary to determine the group of an item.
+        private final HashF[] h;
+
+        /* frequencyMatrix[i][j] is the frequency approximation for item j which was computed during the i-th parallel run of the algorithm.
+        * Note that the approximation of an item j can only be returned if the original hash function is known which was used to map j to it's group during the
+        * creation of the approximates. */
+        private final int[][] frequencyMatrix;
+
+
+        FrequencySketch (int[][] frequencyMatrix, HashF[] h) {
+            this.h = h;
+            this.frequencyMatrix = frequencyMatrix;
+        }
+
+        /**
+         * Queries the sketch.
+         *
+         * The results of all parallel executions (rows in sketch) are being compared, the minimal entry in column j
+         * will be returned as all of the approximations must be overestimates.
+
+         * @param j Item whose frequency will be approximated.
+         * @return Approximation of the frequency for the given item.
+         */
+        public int getFrequencyApproximation(int j) {
+            int min = Integer.MAX_VALUE;
+
+            // iterates through the column j and stores the minimal value (best estimate) into min
+            for (int i = 0; i < frequencyMatrix.length; i++) {
+                // item j in the currently observed parallel run i
+                int currentJ = frequencyMatrix[i][h[i].hash(j)];
+
+                if (currentJ < min)
+                    min = currentJ;
+            }
+
+            return min;
+        }
     }
 
     /* Used to generate and encapsulate pairwise independent hash functions.
